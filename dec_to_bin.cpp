55bd966e5575f3ea61f1bde8c0c03f87acbce32b
ebbvc <94893686+ebbvc@users.noreply.github.com>
2023-12-06 10:39:11 +0100

    dectobinnew
    po naprawie

--- dec_to_bin.cpp
+++ dec_to_bin.cpp
@@ -5,13 +5,11 @@ int decin(int liczba)
 {
 	int i = 0;
 	int tab[15];
-	int j;
 	while (liczba > 0) {
 		tab[i++] = liczba % 2;
 		liczba = liczba / 2;
-		cout << liczba;
 	}
-	for (j = i - 1; j >= 0; j = j - 1); {
+	for (int j = i - 1; j >= 0; j = j - 1) {
 		cout << tab[j];
 	}
 	return 0;
@@ -20,8 +18,8 @@ int main()
 {
     int liczba;
     cout << "Podaj liczbe: "; cin >> liczba;
-	cout << "Liczba" + liczba;
-	cout << "po zamianie to" + decin(liczba);
+	cout << "po zamianie to: ";
+	decin(liczba);
 }
 
 
